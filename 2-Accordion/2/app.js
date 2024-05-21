3a494bfcba6d1ae60b110f484392713b56fe6f0a
Hossein <hosseinnoruzpur@outlook.com>
2024-05-21 21:12:32 +0330

    DOM

--- 2-Accordion/2/app.js
+++ 2-Accordion/2/app.js
@@ -1,3 +1,30 @@
-const TITLE = document.querySelectorAll("accordion_title");
-const CAPTION = document.querySelectorAll("accordion_caption");
+const TITLE = document.querySelectorAll(".accordion_title");
+const CAPTION = document.querySelectorAll(".accordion_caption");
 const ICON = document.querySelectorAll("i");
+
+TITLE[0].addEventListener("click" , function() {
+  CAPTION[0].classList.toggle("open");
+  ICON[0].classList.toggle("rotate");
+  CAPTION[1].classList.remove("open");
+  ICON[1].classList.toggle("rotate");
+  CAPTION[2].classList.remove("open");
+  ICON[2].classList.toggle("rotate");
+})
+
+TITLE[1].addEventListener("click" , function() {
+  CAPTION[1].classList.toggle("open");
+  ICON[1].classList.toggle("rotate");
+  CAPTION[0].classList.remove("open");
+  ICON[0].classList.toggle("rotate");
+  CAPTION[2].classList.remove("open");
+  ICON[2].classList.toggle("rotate");
+})
+
+TITLE[2].addEventListener("click" , function() {
+  CAPTION[2].classList.toggle("open");
+  ICON[2].classList.toggle("rotate");
+  CAPTION[1].classList.remove("open");
+  ICON[1].classList.toggle("rotate");
+  CAPTION[0].classList.remove("open");
+  ICON[0].classList.toggle("rotate");
+})
